d1010cf818d691d4767504648c71546c2182c10a
Simon Horlick <simonhorlick@gmail.com>
2019-10-22 13:15:23 +0800

    Include the spec file so the checker actually works

--- helloruby/__init__.py
+++ helloruby/__init__.py
@@ -4,4 +4,5 @@ import check50
 @check50.check()
 def hello_world():
     """hello world"""
+    check50.include("hello_spec.rb")
     check50.run("rspec --no-colour hello_spec.rb").stdout("0 failures").exit(0)
